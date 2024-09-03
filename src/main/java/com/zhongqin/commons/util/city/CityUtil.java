bdf19579c8389b07d293115882c7f7ad719f5bf7
Kevin <1944741442@qq.com>
2024-09-03 09:22:21 +0800

    1、根据车牌定位省

--- src/main/java/com/zhongqin/commons/util/city/CityUtil.java
+++ src/main/java/com/zhongqin/commons/util/city/CityUtil.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zhongqin.commons.util.HttpsUtil;
 
+import java.util.HashMap;
+
 /**
  * @author Kevin
  * IP数据云 获取IP城市
@@ -51,6 +53,49 @@ public class CityUtil {
             }
         }
         return replace.replaceAll("市", "");
+    }
+
+
+    public static String getProvincialLevel(String city) {
+        if (city.length() > 1) {
+            city = String.valueOf(city.charAt(0));
+        }
+        HashMap<String, String> map = new HashMap<>();
+        map.put("皖", "安徽");
+        map.put("澳", "澳门");
+        map.put("港", "香港");
+        map.put("台", "台湾");
+        map.put("京", "北京");
+        map.put("沪", "上海");
+        map.put("津", "天津");
+        map.put("渝", "重庆");
+        map.put("冀", "河北省");
+        map.put("豫", "河南");
+        map.put("云", "云南");
+        map.put("辽", "辽宁");
+        map.put("黑", "黑龙江");
+        map.put("湘", "湖南");
+        map.put("鲁", "山东");
+        map.put("新", "新疆维吾尔自治区");
+        map.put("苏", "江苏");
+        map.put("浙", "浙江");
+        map.put("赣", "江西");
+        map.put("鄂", "湖北");
+        map.put("桂", "广西壮族自治区");
+        map.put("甘", "甘肃省");
+        map.put("晋", "山西");
+        map.put("蒙", "内蒙古自治区");
+        map.put("陕", "陕西");
+        map.put("吉", "吉林");
+        map.put("闽", "福建");
+        map.put("贵", "贵州");
+        map.put("粤", "广东");
+        map.put("川", "四川");
+        map.put("青", "青海");
+        map.put("藏", "西藏自治区");
+        map.put("琼", "海南");
+        map.put("宁", "宁夏回族自治区");
+        return map.get(city);
     }
 
 }
